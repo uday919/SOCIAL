d7ac883a5818440b345e27ac642e751e8123a910
uday919 <udaykiran919p@gmail.com>
2024-09-30 15:55:04 -0500

    API

---
+++ cmd/api/api.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) run() error {
+	mux := http.NewServeMux()
+	srv := &http.Server{
+		Addr:    app.config.addr,
+		Handler: mux,
+	}
+	log.Printf("Server has started at %s", app.config.addr)
+	return srv.ListenAndServe()
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/uday919/social
+
+go 1.22.0
