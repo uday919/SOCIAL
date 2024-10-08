fed7cd38fbba42af12a0a5f04c6aa693040dee27
uday919 <udaykiran919p@gmail.com>
2024-10-08 09:54:54 -0500

    update

--- cmd/api/Health.go
+++ cmd/api/Health.go
@@ -4,4 +4,5 @@ import "net/http"
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("ok"))
+	app.store.Posts.Create(r.Context())
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,22 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/uday919/social/internal/env"
+	"github.com/uday919/social/internal/store"
+)
 
 func main() {
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
+	store := store.NewStorage(nil)
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
+
 	mux := app.mount()
 	log.Fatal(app.run(mux))
 }
